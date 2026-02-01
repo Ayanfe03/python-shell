8624e8deb856a6e0b290ce61857a0202b0ec6a0e
Oluwasuan Ayanfeoluwa <ayanfe1711@gmail.com>
2026-02-01 02:03:05 +0100

    reverted back to before PATH

--- app/main.py
+++ app/main.py
@@ -1,14 +1,5 @@
 import sys
 
-paths = os.environ["PATH"].split(":")
-
-
-def get_executable_from_path(program) -> str | None:
-    for path in paths:
-        exe = os.path.join(path, program)
-        if os.path.isfile(exe) and os.access(exe, os.X_OK):
-            return exe
-
 def main():
 
     built_in_commands = ["echo", "type", "exit"]
@@ -31,12 +22,6 @@ def main():
                 print(f"{new_command[1]} is a shell builtin")
             elif new_command[1] not in built_in_commands:
                 print(f"{new_command[1]}: not found")
-            else:
-                exe = get_executable_from_path(program)
-                if exe:
-                    print(f"{program} is {exe}")
-                else:
-                    print(f"{program}: not found")
         else:
             print(f"{command}: command not found")
 
